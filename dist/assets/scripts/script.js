fd284e97a345276a56c0d6702c819d322e313434
Faderr J <johmfaderr@gmail.com>
2019-07-15 20:42:55 +0000

    mobile layout and design missing middle layout to be responsive

--- dist/assets/scripts/script.js
+++ dist/assets/scripts/script.js
@@ -15,36 +15,40 @@ $(window).scroll(function () {
   }
 });
 
-// change navbar left text color on scroll
-$(window).scroll(function () {
-  if ($(window).scrollTop() >= 2) {
-    $('.navbar-left').css('color','#064167');
-  } else {
-    $('.navbar-left').css('color','#FBFBFB');
+window.addEventListener('resize', function(){
+  if(window.innerWidth > 767){
+    // change navbar left text color on scroll
+    $(window).scroll(function () {
+      if ($(window).scrollTop() >= 2) {
+        $('.navbar-left').css('color','#064167');
+      } else {
+        $('.navbar-left').css('color','#FBFBFB');
+      }
+    });
+
+    // change navbar logo on scroll
+    var initialSrc = "assets/img/saada-logo-white.png";
+    var scrollSrc = "assets/img/saada-logo.png";
+
+    $(window).scroll(function() {
+      var value = $(this).scrollTop();
+      if (value > 2)
+          $(".logo").attr("src", scrollSrc);
+      else
+          $(".logo").attr("src", initialSrc);
+    });
+
+    // change usecase tab backgroung image on click
+    $(document).ready(function (){
+      $("#events-tab").on("click",function() {
+        $("#usecase").css({"background-image":"url('/dist/assets/img/pictures/concert-01.jpg')"});
+      });
+      $("#entertainment-tab").on("click",function() {
+        $("#usecase").css({"background-image":"url('/dist/assets/img/pictures/cinema-01.jpg')"});
+      });
+      $("#travel-tab").on("click",function() {
+        $("#usecase").css({"background-image":"url('/dist/assets/img/pictures/travel-01.jpg')"});
+      });
+    });
   }
-});
-
-// change navbar logo on scroll
-var initialSrc = "assets/img/saada-logo-white.png";
-var scrollSrc = "assets/img/saada-logo.png";
-
-$(window).scroll(function() {
-   var value = $(this).scrollTop();
-   if (value > 2)
-      $(".logo").attr("src", scrollSrc);
-   else
-      $(".logo").attr("src", initialSrc);
-});
-
-// change usecase tab backgroung image on click
-$(document).ready(function (){
-  $("#events-tab").on("click",function() {
-    $("#usecase").css({"background-image":"url('/dist/assets/img/pictures/concert-01.jpg')"});
-  });
-  $("#entertainment-tab").on("click",function() {
-    $("#usecase").css({"background-image":"url('/dist/assets/img/pictures/cinema-01.jpg')"});
-  });
-  $("#travel-tab").on("click",function() {
-    $("#usecase").css({"background-image":"url('/dist/assets/img/pictures/travel-01.jpg')"});
-  });
 });
